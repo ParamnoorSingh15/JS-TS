2ade9292b5ac5a1ede40f0ddf4ba3b85e6f5b372
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-04-12 00:18:53 +0530

    operators in js

---
+++ Javascript/modules/Basics/operators.js
@@ -0,0 +1,72 @@
+// Arithmetic, comparison, logical, assignment, unary, ternary
+
+// + - * / % **
+// = == ===
+// < > <= >= ! != !== !!
+// && || !
+// ?:
+
+// Arithmetic operator
+
+console.log(1+2); // addition
+console.log('1' + '2'); // concatenation
+
+console.log(12 - 6); // subtraction
+console.log(12 * 6); // multiplication
+console.log(12 % 3); // remainder, modulation operation
+console.log(2**3); // power operator
+
+// Comaprison operator
+console.log(12 == '12'); // it checks only value
+console.log(12 === '12'); // it checks value and type also
+console.log(13 >= 10); // greater than or equal to
+console.log(12 <= 12); // less than or equal to
+console.log(12 != '13'); // not equal to (not strict)
+console.log(12 != '12'); // not equal to with type checking also (strict)
+
+
+let a = 8; // assignment operator
+
+// += -= *= %= /= 
+
+// Logical operator
+
+// true && true -> true
+// true && false -> false
+// false && true -> false
+// false && false -> false  
+
+// true || true -> true
+// true || false -> true
+// false || true -> true
+// false || false -> false   
+
+// ++ -- , increment and decrement operator 
+// a++ means a=a+1 and a-- means a=a-1
+
+// ++a is preincrement , increase value then print
+// a++ is postincrement , print value then increase
+
+
+// Ternary operator
+// condition ? true hui ka code : false hui ka code
+
+a = 10;
+b = 20;
+
+ans = (a>b)?a : b;
+console.log(ans);
+
+// instanceof means uska vanshaj, kya usse juda hua ha , uski peedi basically
+// basically to check if it joined with class , objects ,,, it works only with reference types
+
+
+
+
+
+
+
+
+
+
+
